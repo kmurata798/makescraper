abfe107e94608939734dc09fa0581a2f5df24d31
kmurata798 <kmurata798@gmail.com>
2020-02-18 16:35:56 -0800

    Makescraper.go to scrape data, store data into a struct, serialize the struct to be read from json, and write scraped data stored in json struct into output.json file :

--- scrape.go
+++ scrape.go
@@ -11,7 +11,7 @@ import (
 
 // Struct that can be used for json
 type scrapedData struct {
-	Content string `json:"content"`
+	Content []string `json:"content"`
 }
 
 func writeFile(name string, data string) {
@@ -34,31 +34,33 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
+	var r []string
 	// On every a element which has href attribute call callback
 	c.OnHTML("#mp-tfa > p", func(e *colly.HTMLElement) {
 
 		// Print link
-		data := scrapedData{Content: e.Text}
-		fmt.Println(data)
+		r = append(r, e.Text)
+		data1 := scrapedData{Content: r}
+		fmt.Println(data1)
 
-		// scrapedJSON, _ := json.Marshal(data)
-		scrapedJSON1, _ := json.MarshalIndent(data, "", "    ")
-		fmt.Println(string(scrapedJSON1))
+		// scrapedJSON1, _ := json.Marshal(data)
+		scrapedJSON, _ := json.MarshalIndent(data1, "", "    ")
+		fmt.Println(string(scrapedJSON))
 
-		writeFile(arg, string(scrapedJSON1))
 	})
 
 	c.OnHTML("#mp-itn", func(e *colly.HTMLElement) {
 
 		// Print link
-		data := scrapedData{Content: e.Text}
-		fmt.Println(data)
+		r = append(r, e.Text)
+		data2 := scrapedData{Content: r}
+		fmt.Println(data2)
 
-		// scrapedJSON, _ := json.Marshal(data)
-		scrapedJSON2, _ := json.MarshalIndent(data, "", "    ")
+		// scrapedJSON2, _ := json.Marshal(data)
+		scrapedJSON2, _ := json.MarshalIndent(data2, "", "    ")
 		fmt.Println(string(scrapedJSON2))
 
-		// writeFile(arg, string(scrapedJSON2))
+		writeFile(arg, string(scrapedJSON2))
 	})
 
 	// Before making a request print "Visiting ..."
