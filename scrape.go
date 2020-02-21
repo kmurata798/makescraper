149bee7c6f7ee52371df02902bc8d73e2c6c3e79
kmurata798 <kmurata798@gmail.com>
2020-02-20 21:01:48 -0800

    [Add] Echo Server displaying my json

--- scrape.go
+++ scrape.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/gocolly/colly"
+	"github.com/labstack/echo"
 )
 
 // Struct that can be used for json
@@ -14,11 +16,13 @@ type scrapedData struct {
 	Content []string `json:"content"`
 }
 
+type jsonData struct {
+	message []string `json:"message"`
+}
+
 func writeFile(name string, data string) {
 	/*
-		Makesite MVP
-
-			Writes data onto file
+		Writes data onto file
 	*/
 	bytesToWrite := []byte(data)
 	err := ioutil.WriteFile(name, bytesToWrite, 0644)
@@ -29,19 +33,43 @@ func writeFile(name string, data string) {
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
+
+func readFile(name string) string {
+	/*
+		Collects data from file
+	*/
+	fileContents, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	return string(fileContents)
+
+}
+
+func startServer(dataSlice []string) {
+	e := echo.New()
+
+	e.GET("/", func(f echo.Context) error {
+		return f.JSON(http.StatusOK, dataSlice)
+	})
+
+	fmt.Println("Server running: http://localhost:8000")
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
 func main() {
 	arg := os.Args[1]
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	var r []string
+	var dataSlice []string
 	// On every a element which has href attribute call callback
 	// wikipedia new featured article selector
 	c.OnHTML("#mp-tfa > p", func(e *colly.HTMLElement) {
 
 		// Print link
-		r = append(r, e.Text)
-		data1 := scrapedData{Content: r}
+		dataSlice = append(dataSlice, e.Text)
+		data1 := scrapedData{Content: dataSlice}
 		fmt.Println(data1)
 
 		// scrapedJSON, _ := json.Marshal(data1)
@@ -53,8 +81,8 @@ func main() {
 	c.OnHTML("#mp-itn", func(e *colly.HTMLElement) {
 
 		// Print link
-		r = append(r, e.Text)
-		data2 := scrapedData{Content: r}
+		dataSlice = append(dataSlice, e.Text)
+		data2 := scrapedData{Content: dataSlice}
 		fmt.Println(data2)
 
 		// scrapedJSON2, _ := json.Marshal(data2)
@@ -71,5 +99,8 @@ func main() {
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://en.wikipedia.org/wiki/Main_Page")
+
+	// fileContents := readFile(arg)
+	startServer(dataSlice)
 
 }
